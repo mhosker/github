9b991fe29ea626c4ef565b461038945175e12b04
Mike Hosker <37193543+mhosker@users.noreply.github.com>
2026-05-24 18:20:53 +0100

    DO spaces backend settings

--- mhosker.tf
+++ mhosker.tf
@@ -20,6 +20,8 @@ terraform {
     skip_metadata_api_check     = true
     skip_region_validation      = true
     skip_s3_checksum            = true
+    skip_get_ec2_platforms      = true
+    use_path_style              = true
     region                      = "eu-west-2"
   }
 }
